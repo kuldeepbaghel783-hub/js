98dd8538efa23cf7798bd1a992bb4ee0914ed6bc
kuldeepbaghel783-hub <kuldeepbaghel783@gmail.com>
2026-06-16 16:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 
+let P = 100000;
+let r = 0.10;   
+let n = 1;     
+let t = 3;     
 
-let P = 50000; 
-let r = 5;     
-let n = 1;    
-let t = 3;    
 
-
-let A = P * Math.pow((1 + (r / (100 * n))), (n * t));
-
+let A = P * (1 + r / n) ** (n * t);
 
 let CI = A - P;
 
-  
-console.log("The compound interest after 3 years is:", Math.round(CI));
+
+console.log("The compound interest after 3 years is:", CI);
