b9dcc1463d34db6567a688e6e7d521e008398bc2
kuldeepbaghel783-hub <kuldeepbaghel783@gmail.com>
2026-06-16 14:58:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let a = 5;
+let b = 10;
+
+
+
+
+    if ( b%2 ) {
+
+        console.log(" no 10 is even ");
+        
+    }
+    else {
+    console.log(" no 10 is odd");
+    
+}
+
+if (a%2 ) {
+
+    console.log("no 5 is even ");
+    
+    
+} else {
+
+    console.log("no 5 is odd");
+    
+    
+}
